227581b87b0b648d2f3931bd83a775134efcfada
Erkan <ErkanOzulu@users.noreply.github.com>
2023-09-17 18:47:51 +0300

    findById

--- src/main/java/com/bank/repository/AccountRepository.java
+++ src/main/java/com/bank/repository/AccountRepository.java
@@ -1,10 +1,14 @@
 package com.bank.repository;
 
+import com.bank.exception.BadRequestException;
+import com.bank.exception.RecordNotFoundException;
 import com.bank.model.Account;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
+
 @Component
 public class AccountRepository {
 
@@ -18,4 +22,15 @@ public class AccountRepository {
     public List<Account> findAll() {
         return accountList;
     }
-}
+
+    public Account findById(UUID accountId) {
+        //TASK
+        //complete the  method, that find  the account inside the list, if not throw RecordNotFoundException
+
+           return accountList.stream().filter(account->account.getAccountId().equals(accountId))
+                   .findAny().orElseThrow(()->new RecordNotFoundException("account doesn't exist in database"));
+        }
+
+
+    }
+
