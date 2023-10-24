c206fa74bd18675d8209c26c48b53448297fbd4a
Erkan <ErkanOzulu@users.noreply.github.com>
2023-10-24 18:17:49 +0300

    validation

--- src/main/java/com/bank/model/Account.java
+++ src/main/java/com/bank/model/Account.java
@@ -5,7 +5,10 @@ import com.bank.enums.AccountType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.UUID;
@@ -15,9 +18,13 @@ import java.util.UUID;
 public class Account {
 
     private UUID accountId;
+    @NotNull
+    @Positive
     private BigDecimal balance;
+    @NotNull
     private AccountType accountType;
     private Date creationDate;
+    @NotNull
     private Long userId;
     private AccountStatus accountStatus;
 
