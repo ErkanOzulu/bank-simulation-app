0665eb2662b4b6306307a019f5aa13e02ef92eb7
Erkan <ErkanOzulu@users.noreply.github.com>
2024-01-01 23:07:41 +0300

    SecurityConfig and AuthSuccessHandler is completed

--- src/main/java/com/bank/config/AuthSuccessHandler.java
+++ src/main/java/com/bank/config/AuthSuccessHandler.java
@@ -1,5 +1,6 @@
 package com.bank.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -9,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Set;
-
+@Configuration
 public class AuthSuccessHandler implements AuthenticationSuccessHandler {
 
 
